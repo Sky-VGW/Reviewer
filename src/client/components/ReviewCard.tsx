e2a62764e83b7e43503b9696200cef2602a3b148
Sky Weiner <schuyler942@gmail.com>
2025-02-16 11:02:20 -0500

    update styling for reviewCard

--- src/client/components/ReviewCard.tsx
+++ src/client/components/ReviewCard.tsx
@@ -7,15 +7,23 @@ interface ReviewCardProps {
 
 const ReviewCard: React.FC<ReviewCardProps> = ({ author, score, timestamp, content }) => {
   return (
-    <div className="review-card" style={{border: '1px solid black', padding: '10px', margin: '10px', borderRadius: '5px'}}>
+    <div className="review-card" 
+      style={{
+        border: '1px solid black',
+        padding: '10px',
+        margin: '10px',
+        borderRadius: '5px',
+        boxShadow: '6px 4px 4px darkgray'
+      }}
+    >
       <div className="review-header">
-        <span className="author"><strong>{author}</strong></span>
-        <span className="score"> {'⭐'.repeat(score || 0)}</span>
-        <p className="timestamp">
+        <span className="review-author"><strong>{author} - </strong></span>
+        <span className="review-score"> {'⭐'.repeat(score || 0)}</span>
+        <p className="review-timestamp" style={{color: 'gray', marginTop: '0px', fontSize: '12px'}}>
           {new Date(timestamp).toLocaleString()}
         </p>
       </div>
-      <p className="content">{content}</p>
+      <p className="review-content">{content}</p>
     </div>
   );
 };
